0855abaa3e7c33d41303f7e4bddbf036dd056cf1
AsyncIns <47288304+asyncins@users.noreply.github.com>
2019-09-24 18:56:32 +0800

    Update predict.py
    微调，适配本书

--- predict.py
+++ predict.py
@@ -12,13 +12,9 @@ predicts = list(Path(PATH_PREDICTS).glob('*.{}'.format(IMAGE_TYPE)))
 if not os.path.isdir(PATH_OUT):
     # out目录存储输出图片，如果目录不存在则创建
     os.makedirs(PATH_OUT)
-if not os.path.isdir(PATH_COO):
-    # coo目录存储输出坐标，如果目录不存在则创建
-    os.makedirs(PATH_COO)
 # 循环检测
 for i in predicts:
     out = os.path.join(PATH_OUT, str(i.name))
-    coo = os.path.join(PATH_COO, str(i.stem) + '.txt')
     command = 'cd darknet && ./darknet detector {instruct} data/{data} cfg/{cfg}' \
               ' weights/{weights} -out {out} {i} -thresh 0.5 -gpus 0 ' \
               .format(instruct=instruct, data=data, cfg=cfg,
